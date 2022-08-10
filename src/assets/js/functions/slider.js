5ef7abc96de67c1d99422218dba72cc56bc87127
Irina Burykh <diva84@mail.ru>
2022-08-11 00:04:50 +0300

    tariff

--- src/assets/js/functions/slider.js
+++ src/assets/js/functions/slider.js
@@ -7,4 +7,18 @@ window.addEventListener('DOMContentLoaded', function() {
 		initialSlide: 0,
 		slidesPerView: 'auto'
 	});
+
+	const tableSlider = new Swiper('.slider-table', {
+		pagination: {
+			el: ".swiper-pagination",
+			type: 'bullets',
+			clickable: true,
+		  },
+		grabCursor: true,
+		spaceBetween: 32,
+		slidesPerGroup: 1,
+		centeredSlides: true,
+		initialSlide: 1,
+		slidesPerView: 'auto'
+	});
 });
