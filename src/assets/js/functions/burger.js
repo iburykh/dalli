26b4fd028fc44792444deca940ba7fdf99b0526b
Irina Burykh <diva84@mail.ru>
2022-08-10 20:32:47 +0300

    about

---
+++ src/assets/js/functions/burger.js
@@ -0,0 +1,33 @@
+let menuBody = document.querySelector('.burger-menu');
+let menuItem = menuBody.querySelectorAll('.menu__list > li');
+let burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {    
+    burger.classList.toggle('active');
+    menuBody.classList.toggle('active');
+    if (burger.classList.contains('active')) {
+        burger.setAttribute('aria-label', 'закрыть навигацию');
+        disableScroll();
+    } else {
+        burger.setAttribute('aria-label', 'открыть навигацию');
+        enableScroll();
+    }
+
+    setTimeout(() => {
+        menuBody.focus();
+    }, 600);
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        
+        if (burger.classList.contains('active')) {
+            burger.classList.remove('active');
+            menuBody.classList.remove('active');
+            burger.setAttribute('aria-label', 'открыть навигацию');
+            enableScroll();
+        }
+    })
+})
---
+++ src/assets/js/functions/slider.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    const mainSlider = new Swiper('.swiper', {
+		grabCursor: true,
+		spaceBetween: 32,
+		slidesPerGroup: 1,
+		initialSlide: 0,
+		slidesPerView: 'auto'
+	});
+});
